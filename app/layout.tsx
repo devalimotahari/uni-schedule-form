9345a3dd00c79a37a8a08afaa5e3cf99bbaa8145
AliMotahariPour <devalimotahari@gmail.com>
2024-10-25 17:31:45 +0330

    migrate to next 15.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
 
 export const metadata = {
   metadataBase: new URL('https://postgres-starter.vercel.app'),
   title: 'Vercel Postgres Demo',
+
   description:
-    'A simple Next.js app with Vercel Postgres as the database',
+    'A simple Next.js app with Vercel Postgres as the database'
 }
 
 const inter = Inter({
